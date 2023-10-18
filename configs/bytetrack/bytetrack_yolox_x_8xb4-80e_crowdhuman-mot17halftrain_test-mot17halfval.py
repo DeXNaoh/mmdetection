945f6423cb9c752a451f558a5de11bd25d606262
DeXNaoh <146802261+DeXNaoh@users.noreply.github.com>
2023-10-18 18:27:17 +0900

    Update bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
    corrected

--- configs/bytetrack/bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
+++ configs/bytetrack/bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
@@ -83,7 +83,7 @@ test_pipeline = [
                 size_divisor=32,
                 pad_val=dict(img=(114.0, 114.0, 114.0))),
             dict(type='LoadTrackAnnotations'),
-         )],
+         ]),
     dict(type='PackTrackInputs')
 ]
 train_dataloader = dict(
