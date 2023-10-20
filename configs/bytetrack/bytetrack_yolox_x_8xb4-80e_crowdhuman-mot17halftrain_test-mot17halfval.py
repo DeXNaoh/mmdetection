747f0ec80bbfbaa7c4c254f2a5602f6b0e6b190d
DeXNaoh <146802261+DeXNaoh@users.noreply.github.com>
2023-10-20 17:23:56 +0900

    Update bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
    oay okay

--- configs/bytetrack/bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
+++ configs/bytetrack/bytetrack_yolox_x_8xb4-80e_crowdhuman-mot17halftrain_test-mot17halfval.py
@@ -74,15 +74,19 @@ train_pipeline = [
 ]
 
 test_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=_base_.backend_args),
-    dict(type='Resize', scale=img_scale, backend='pillow', keep_ratio=True),
     dict(
-        type='Pad',
-        size_divisor=32,
-        pad_val=dict(img=(114.0, 114.0, 114.0))),
-    dict(type='LoadTrackAnnotations'),
+        type='TransformBroadcaster',
+        transforms=[
+            dict(type='LoadImageFromFile', backend_args=_base_.backend_args),
+            dict(type='Resize', scale=img_scale, keep_ratio=True),
+            dict(
+                type='Pad',
+                size_divisor=32,
+                pad_val=dict(img=(114.0, 114.0, 114.0))),
+            dict(type='LoadTrackAnnotations'),
+        ]),
     dict(type='PackTrackInputs')
-]    
+]   
 train_dataloader = dict(
     _delete_=True,
     batch_size=batch_size,
